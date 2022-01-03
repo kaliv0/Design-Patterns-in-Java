428fb96e086f4b08dfbae2939f2cd45d000ecf5f
Kaloyan Ivanov <kaloyan.ivanov88@gmail.com>
2022-01-03 10:55:56 +0200

    Create Captain.java

---
+++ Creational patterns/Singleton/Captain.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public final class Captain {
+
+    private static Captain captain;
+
+    private Captain() {
+    }
+
+    public static synchronized Captain getCaptain() {
+
+        if (captain == null) {
+
+            captain = new Captain();
+            System.out.println("New captain is elected for your team");
+
+        } else {
+
+            System.out.println("Your team already has a captain");
+        }
+
+        return captain;
+    }
+}
