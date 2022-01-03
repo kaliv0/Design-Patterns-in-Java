da01a6f68d3e2c4ac3ceffa7c6bc749e9c011f9d
Kaloyan Ivanov <kaloyan.ivanov88@gmail.com>
2022-01-03 19:59:19 +0200

    Add files via upload

---
+++ Creational patterns/Builder (other version)/Vehicle.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.*;
+
+public class Vehicle {
+
+    private List<String> parts;
+
+    //ctor
+    public Vehicle() {
+        this.parts = new ArrayList<String>();
+    }
+
+    //methods
+    public void add(String part) {
+
+        this.parts.add(part);
+    }
+
+
+    public void showVehicle() {
+
+        System.out.println("\nVehicle completed as below : ");
+
+        for (String part : parts) {
+            System.out.println(part);
+        }
+    }
+
+
+}
