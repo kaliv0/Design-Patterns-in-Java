719279842fb870f71d509f17927dfb2d77fbb230
Kaloyan Ivanov <kaloyan.ivanov88@gmail.com>
2022-01-03 19:02:43 +0200

    Update Main.java

--- Creational patterns/Builder/Main.java
+++ Creational patterns/Builder/Main.java
@@ -1,4 +1,24 @@
 /*
 Separate the construction of a complex object from its representation so that the same 
 construction processes can create different representations
+
+Builder pattern builds complex object using a step by step approach
 */
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Beverage tea = HotelWaiter.takeOrder("tea");
+        System.out.println(tea);
+
+        Beverage coffee = HotelWaiter.takeOrder("coffee");
+        System.out.println(coffee);
+
+        Beverage water = HotelWaiter.takeOrder("water");
+        //toString method will display "null"
+    }
+}
+
