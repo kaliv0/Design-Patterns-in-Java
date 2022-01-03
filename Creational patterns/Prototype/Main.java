dd236cc612f9aec31c67abbde33d9c0e8daa5f17
Kaloyan Ivanov <kaloyan.ivanov88@gmail.com>
2022-01-03 14:52:35 +0200

    Update Main.java

--- Creational patterns/Prototype/Main.java
+++ Creational patterns/Prototype/Main.java
@@ -1,3 +1,5 @@
+//GoG defitnion => Specify the kinds of objects to create using a prototypical instance, and create new objects by copying this prototype.
+
 package com.company;
 
 import java.util.Scanner;
