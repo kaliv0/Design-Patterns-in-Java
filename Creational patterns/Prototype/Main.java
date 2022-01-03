d1e355fb0a7005162a997951c6aab5d10685114e
Kaloyan Ivanov <kaloyan.ivanov88@gmail.com>
2022-01-03 14:38:27 +0200

    Update Main.java

--- Creational patterns/Prototype/Main.java
+++ Creational patterns/Prototype/Main.java
@@ -1,15 +1,38 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) throws CloneNotSupportedException { //You need to add exception to signature
 
-        Piano piano = new Piano("Yamaha");
-        System.out.printf("Your %s has %s keys%n", piano.getModelName(), piano.getNumberOfKeys());
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter model name:");
+        String modelName = sc.nextLine();
+
+        System.out.println("Enter number of keys:");
+        int numberOfKeys = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Enter color:");
+        String color = sc.nextLine();
+
+        System.out.println("Enter price:");
+        int price = Integer.parseInt(sc.nextLine());
+
+        Piano piano = new Piano(modelName, numberOfKeys, color, price);
+        piano.displayInfo();
 
         Piano biggerPiano = piano.clone();
-        biggerPiano.increaseNumberOfKeys(24);
-        System.out.printf("Your %s has %s keys%n", biggerPiano.getModelName(), biggerPiano.getNumberOfKeys());
+
+        int addAmount;
+        System.out.println("Enter amount: "); //selling at a higher price for particular reason
+
+        addAmount = Integer.parseInt(sc.next());
+        biggerPiano.increasePrice(addAmount);
+
+        biggerPiano.displayInfo();
+        piano.displayInfo(); //first one hasn't changed
 
 
     }
