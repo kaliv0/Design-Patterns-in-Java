39e39693d7d88b1e695a9c2494b8f65459896088
Kaloyan Ivanov <kaloyan.ivanov88@gmail.com>
2022-01-03 14:54:02 +0200

    Update Main.java

--- Creational patterns/Factory/Main.java
+++ Creational patterns/Factory/Main.java
@@ -1,3 +1,8 @@
+/*
+Define an interface for creating an object, but let subclasses decide which class to instantiate.
+Factory method lets a class defer instantiation to subclasses.
+*/
+
 package com.company;
 
 public class Main {
