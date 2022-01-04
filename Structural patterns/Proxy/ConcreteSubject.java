f934970b9048c9efb38d87a94337ffd3ba194d2d
Kaloyan Ivanov <kaloyan.ivanov88@gmail.com>
2022-01-04 23:58:53 +0200

    Update ConcreteSubject.java

--- Structural patterns/Proxy/ConcreteSubject.java
+++ Structural patterns/Proxy/ConcreteSubject.java
@@ -2,7 +2,7 @@ package protectionProxy;
 
 public class ConcreteSubject extends Subject {
 
-//nevertheless you could still invoke the method through an instance of that class
+//nevertheless you could still invoke the method through an instance of that class?!
 	@Override
 	protected void doSomething() {
 		System.out.println("doSomeWork() inside ConcreteSubject is invoked.");
