4cdbbe3348da0b534cad43924f3a8203d24d9bfe
Kaloyan Ivanov <kaloyan.ivanov88@gmail.com>
2022-05-07 11:29:52 +0300

    update comment

--- Behavioral patterns/ChainOfResponsibility/src/Client.java
+++ Behavioral patterns/ChainOfResponsibility/src/Client.java
@@ -1,3 +1,9 @@
+/*
+    Chain of Responsibility lets you pass requests along a chain of handlers.
+    Upon receiving a request, each handler decides either to process the request
+    or to pass it to the next handler in the chain.
+*/
+
 import models.*;
 
 public class Client {
