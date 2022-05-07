e2dd31e50911d450b6509f458feaa8080166484f
Kaloyan Ivanov <kaloyan.ivanov88@gmail.com>
2022-05-07 11:30:24 +0300

    update comment

--- Behavioral patterns/Observer/src/Client.java
+++ Behavioral patterns/Observer/src/Client.java
@@ -1,6 +1,6 @@
 /*
-Observer pattern lets you define a subscription mechanism to notify multiple objects
-about any events that happen to the object they’re observing.
+    Observer pattern lets you define a subscription mechanism to notify multiple objects
+    about any events that happen to the object they’re observing.
  */
 import models.Bank;
 import models.customers.*;
